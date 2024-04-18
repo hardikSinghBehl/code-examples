d82dd09daa07d6c90cc94b45853c12d95ab715ef
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-18 09:32:28 +0530

    adding sqs listener for subscriber microservice

---
+++ aws/spring-cloud-sns-sqs-pubsub/notification-dispatcher-service/src/main/java/io/reflectoring/listener/EmailNotificationListener.java
@@ -0,0 +1,19 @@
+package io.reflectoring.listener;
+
+import org.springframework.stereotype.Component;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import io.awspring.cloud.sqs.annotation.SnsNotificationMessage;
+import io.reflectoring.dto.UserCreatedEventDto;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class EmailNotificationListener {
+
+	@SqsListener("${io.reflectoring.aws.sqs.queue-name}")
+	public void listen(@SnsNotificationMessage final UserCreatedEventDto userCreatedEvent) {
+		log.info("Dispatching account creation email to {} on {}", userCreatedEvent.getName(), userCreatedEvent.getEmailId());
+		// business logic to send email
+	}
+
+}
