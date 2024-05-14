0816db57702c6ae72f6f42cf8243cabf85b6345b
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-05-14 20:29:49 +0530

    removing unused fields

--- aws/spring-cloud-aws-dynamodb/src/main/java/io/reflectoring/dto/MedicalRecordCreationDto.java
+++ aws/spring-cloud-aws-dynamodb/src/main/java/io/reflectoring/dto/MedicalRecordCreationDto.java
@@ -2,10 +2,8 @@ package io.reflectoring.dto;
 
 public record MedicalRecordCreationDto(
     String patientName,
-    String medicalHistory,
     String diagnosis,
     String treatmentPlan,
-    String allergies,
     String attendingPhysicianName
 ) {
 }
